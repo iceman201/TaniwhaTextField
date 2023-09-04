c6dbf86b15af78603bbb360fe28bee01c4c265b6
Liguo Jiao <liguo.jiao@fiserv.com>
2023-09-04 19:01:42 +1200

    [1.5.2] Update Example

--- Example/TaniwhaTextField/ViewController.swift
+++ Example/TaniwhaTextField/ViewController.swift
@@ -11,14 +11,30 @@ import TaniwhaTextField
 
 class ViewController: UIViewController {
 
-    @IBOutlet var taniwha: TaniwhaTextField!
-    
-    @IBOutlet var marimari: TaniwhaTextField!
+    @IBOutlet weak var taniwha: TaniwhaTextField!
+    @IBOutlet weak var marimari: TaniwhaTextField!
+
+    weak var codingUITextField: TaniwhaTextField?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         taniwha.placeholder = "Tena Koe"
         marimari.placeholder = "Kia Ora"
+
+        let textField = TaniwhaTextField(bottomLineAlphaAfter: 0, placeholderTextColor: .red)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.setPlaceholder("CodingUI Example")
+        textField.placeholderFont = .systemFont(ofSize: 11)
+        textField.font = .systemFont(ofSize: 12)
+        
+        self.view.addSubview(textField)
+        self.codingUITextField = textField
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: marimari.bottomAnchor, constant: 8*3),
+            textField.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 2/3),
+            textField.heightAnchor.constraint(equalToConstant: 39),
+            textField.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        ])
     }
     
     override func didReceiveMemoryWarning() {
