db8f50e7409d75205ae56be096668504a8ea99ca
Liguo Jiao <liguo.jiao@fiserv.com>
2023-09-04 19:01:08 +1200

    [1.5.2] Refactor and stability improvement
    Add coding UI support.

--- TaniwhaTextField/Classes/TaniwhaTextField.swift
+++ TaniwhaTextField/Classes/TaniwhaTextField.swift
@@ -27,28 +27,61 @@
 import UIKit
 
 @IBDesignable open class TaniwhaTextField: UITextField {
-    let scaleIndex : CGFloat = 0.75
-    let textInsetSpace : CGFloat = 1.5
-    let placeholderAlphaAfter : CGFloat = 0.85
-    let placeholderAlphaBefore : CGFloat = 0.5
+    private let scaleIndex : CGFloat = 0.75
+    private let textInsetSpace : CGFloat = 1.5
+    private let placeholderAlphaAfter : CGFloat = 0.85
+    private let placeholderAlphaBefore : CGFloat = 0.5
 
-    var placeholderLabelView = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-    var bottomlineView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-    var isMoveUp = false
-    var isChanged  = false
+    private var placeholderLabelView: UILabel
+    private var bottomlineView: UIView
+    private var isMoveUp = false
+    private var isChanged  = false
+    open var placeholderFont: UIFont?
 
-    @IBInspectable var bottomLineWidth : CGFloat = 2.0
+    @IBInspectable var bottomLineWidth : CGFloat = 1.0
     @IBInspectable var bottomLineColor : UIColor = .black
     @IBInspectable var bottomLineAlphaBefore : CGFloat = 0.5
     @IBInspectable var bottomLineAlphaAfter : CGFloat = 1
     @IBInspectable var placeholderTextColor : UIColor = .gray
     @IBInspectable var animateDuration : TimeInterval = 0.35
 
+    public init(bottomLineWidth: CGFloat = 2.0,
+                bottomLineColor: UIColor = .black,
+                bottomLineAlphaBefore: CGFloat = 0.5,
+                bottomLineAlphaAfter: CGFloat = 1,
+                placeholderTextColor: UIColor = .gray,
+                animateDuration: TimeInterval = 0.35) {
+        self.placeholderLabelView = UILabel(frame: .zero)
+        self.bottomlineView = UIView(frame: .zero)
+        self.bottomLineWidth = bottomLineWidth
+        self.bottomLineColor = bottomLineColor
+        self.bottomLineAlphaBefore = bottomLineAlphaBefore
+        self.bottomLineAlphaAfter = bottomLineAlphaAfter
+        self.placeholderTextColor = placeholderTextColor
+        self.animateDuration = animateDuration
+        super.init(frame: .zero)
+    }
+
+    required public init?(coder: NSCoder) {
+        self.placeholderLabelView = UILabel(frame: .zero)
+        self.bottomlineView = UIView(frame: .zero)
+        super.init(coder: coder)
+    }
+    
+    open func setPlaceholder( _ text: String) {
+        self.placeholder = text
+    }
+
+    open func setText(_ text: String) {
+        self.text = text
+    }
+
     override open func draw(_ rect: CGRect) {
         super.draw(rect)
         self.drawLine()
         NotificationCenter.default.addObserver(self, selector: #selector(didBeginTextEdit), name: UITextField.textDidBeginEditingNotification, object: self)
-        NotificationCenter.default.addObserver(self, selector: #selector(didTextEditFinish), name: UITextField.textDidChangeNotification, object: self)
+        NotificationCenter.default.addObserver(self, selector: #selector(didTextDidChange), name: UITextField.textDidChangeNotification, object: self)
+        NotificationCenter.default.addObserver(self, selector: #selector(didEndTextEdit), name: UITextField.textDidEndEditingNotification, object: self)
     }
 
     override open func drawPlaceholder(in rect: CGRect) {
@@ -62,7 +95,12 @@ import UIKit
         placeholderLabelView.text = self.placeholder
         self.placeholder = nil
 
-        placeholderLabelView.font = UIFont(name: font.fontName, size: font.pointSize)
+        if let placeholderFont = placeholderFont {
+            placeholderLabelView.font = placeholderFont
+        } else {
+            placeholderLabelView.font = UIFont(name: font.fontName, size: font.pointSize)
+        }
+
         placeholderLabelView.textColor = placeholderTextColor
         placeholderLabelView.alpha = placeholderAlphaBefore
 
@@ -122,23 +160,25 @@ import UIKit
     }
 
     // MARK: - Delegate
+    fileprivate func moveUpAnimation() {
+        UIView.animate(withDuration: animateDuration, animations: {
+            self.isChanged = true
+            self.placeholderLabelView.transform = CGAffineTransform(scaleX: self.scaleIndex, y: self.scaleIndex)
+            self.placeholderLabelView.alpha = self.placeholderAlphaAfter
+            self.placeholderLabelView.center = CGPoint(x: self.placeholderLabelView.center.x * self.scaleIndex, y: 0 + self.placeholderLabelView.frame.size.height)
+            self.bottomlineView.alpha = self.bottomLineAlphaAfter
+        }, completion: { (_) in
+            self.isMoveUp = true
+            self.isChanged = false
+        })
+    }
+
     @objc private func didBeginTextEdit() {
         if !self.isMoveUp {
             if self.isChanged {
                 return
             }
-            UIView.animate(withDuration: animateDuration, animations: {
-                self.isChanged = true
-                self.placeholderLabelView.transform = CGAffineTransform(scaleX: self.scaleIndex, y: self.scaleIndex)
-                self.placeholderLabelView.alpha = self.placeholderAlphaAfter
-                self.placeholderLabelView.center = CGPoint(x: self.placeholderLabelView.center.x * self.scaleIndex, y: 0 + self.placeholderLabelView.frame.size.height)
-                self.bottomlineView.alpha = self.bottomLineAlphaAfter
-            }, completion: { (done) in
-                if done {
-                    self.isMoveUp = true
-                    self.isChanged = false
-                }
-            })
+            moveUpAnimation()
         } else {
             UIView.animate(withDuration: animateDuration, animations: {
                 self.bottomlineView.alpha = self.bottomLineAlphaAfter
@@ -146,47 +186,44 @@ import UIKit
         }
     }
 
-    @objc private func didTextEditFinish() {
+    fileprivate func moveDownAnimation() {
+        UIView.animate(withDuration: animateDuration, animations: {
+            self.isChanged = true
+            self.placeholderLabelView.alpha = self.placeholderAlphaBefore
+            self.placeholderLabelView.center = CGPoint(x: self.placeholderLabelView.center.x / self.scaleIndex, y: self.frame.size.height - self.bottomlineView.frame.size.height - self.placeholderLabelView.frame.size.height / 2.0 - 2.0)
+            self.placeholderLabelView.transform = CGAffineTransform.identity
+            self.bottomlineView.alpha = self.bottomLineAlphaBefore
+        }, completion: { (_) in
+            self.isChanged = false
+            self.isMoveUp = false
+        })
+    }
+
+    @objc private func didTextDidChange() {
         if self.isMoveUp {
             if self.text?.count == 0 {
                 if self.isChanged {
                     return
                 }
-                UIView.animate(withDuration: animateDuration, animations: {
-                    self.isChanged = true
-                    self.placeholderLabelView.alpha = self.placeholderAlphaBefore
-                    self.placeholderLabelView.center = CGPoint(x: self.placeholderLabelView.center.x / self.scaleIndex, y: self.frame.size.height - self.bottomlineView.frame.size.height - self.placeholderLabelView.frame.size.height / 2.0 - 2.0)
-                    self.placeholderLabelView.transform = CGAffineTransform.identity
-                    self.bottomlineView.alpha = self.bottomLineAlphaBefore
-                }, completion: { (done) in
-                    if done {
-                        self.isChanged = false
-                        self.isMoveUp = false
-                    }
-                })
+                moveDownAnimation()
             }
         } else {
-            if (self.text?.count)! > 0  {
+            if self.text?.count ?? 0 > 0  {
                 if self.isChanged {
                     return
                 }
-                UIView.animate(withDuration: animateDuration, animations: {
-                    self.isChanged = true
-                    self.placeholderLabelView.transform = CGAffineTransform(scaleX: self.scaleIndex, y: self.scaleIndex)
-                    self.placeholderLabelView.alpha = self.placeholderAlphaAfter
-                    self.placeholderLabelView.center = CGPoint(x: self.placeholderLabelView.center.x * self.scaleIndex, y: 0 + self.placeholderLabelView.frame.size.height)
-                    self.bottomlineView.alpha = self.bottomLineAlphaAfter
-                }, completion: { (done) in
-                    if done {
-                        self.isMoveUp = true
-                        self.isChanged = false
-                    }
-                })
+                moveUpAnimation()
             } else {
                 UIView.animate(withDuration: animateDuration, animations: {
                     self.bottomlineView.alpha = self.bottomLineAlphaBefore
                 })
             }
+        }
+    }
+
+    @objc private func didEndTextEdit() {
+        if isMoveUp, self.text?.count == 0 {
+            moveDownAnimation()
         }
     }
 
